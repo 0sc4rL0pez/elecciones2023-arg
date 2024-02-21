622c9b4b37720c9083543debb81f8975bbcf9f3e
Oscar López <153029225+0sc4rL0pez@users.noreply.github.com>
2024-02-21 18:24:59 -0300

    changing size plot

--- app.py
+++ app.py
@@ -46,8 +46,8 @@ if visualizacion==etapas[1]:
         y='Porcentaje',
         color = alt.Color('Partido').scale(domain=partidos, range=colores).legend(orient='bottom',columns = 3)
     ).properties(
-            width=800,
-            height=350
+            width=700,
+            height=320
         )
     
     st.altair_chart(linea)
