7bcbcf3dbb9a8661abb6f6ea1b8cada7bf648412
Oscar López <153029225+0sc4rL0pez@users.noreply.github.com>
2024-02-21 16:50:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -186,9 +186,9 @@ elif visualizacion==etapas[0]:
     sum_cant_likes = df_megusta['cantidad_likes'].sum()
 
     if unidades1=='Porcentaje':
-        df_cant_publi['cantidad'] = df_cant_publi[df_cant_publi.columns[1]].map(lambda x: round(100*float(x)/sum_cant_pub,1))
+        df_cant_publi['cantidad'] = df_cant_publi[df_cant_publi.columns[0]].map(lambda x: round(100*float(x)/sum_cant_pub,1))
     elif unidades1 == 'miles':
-        df_cant_publi['cantidad'] = df_cant_publi[df_cant_publi.columns[1]].map(lambda x: round(float(x)/1000,1))
+        df_cant_publi['cantidad'] = df_cant_publi[df_cant_publi.columns[0]].map(lambda x: round(float(x)/1000,1))
     
     pie = alt.Chart(df_cant_publi).mark_arc(innerRadius=60,outerRadius=120).encode(
         theta=alt.Theta(field="Partido", type="quantitative",stack=True),
