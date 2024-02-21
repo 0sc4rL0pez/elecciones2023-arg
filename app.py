7783945dfc5a2c3eec4fc35b42953e17611230c1
Oscar López <153029225+0sc4rL0pez@users.noreply.github.com>
2024-02-21 09:13:40 -0300

    pagina datos mejorada

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import pandas as pd
 import numpy as np
 
 
-etapas = ('Primera vuelta', 'Ballotage', 'Datos')
+etapas = ('Datos','Primera vuelta', 'Ballotage')
 visualizacion = st.sidebar.selectbox('Visualizar: ',etapas)
 
 st.header('**Elecciones + IA**')
 
-if visualizacion==etapas[0]:
+if visualizacion==etapas[1]:
     st.subheader('Primera vuelta')
     st.text("Encuestas en función del tiempo (Wikipedia)")
 
@@ -41,7 +41,7 @@ if visualizacion==etapas[0]:
     df_fecha_selec_aux = df_fecha_selec.rename(index={0:'Porcentaje'})
     st.bar_chart(df_fecha_selec[options].T,height=350,use_container_width=True)
 
-elif visualizacion==etapas[1]:
+elif visualizacion==etapas[2]:
     st.subheader('Ballotaje')
     st.text("Encuestas en función del tiempo (Wikipedia)")
 
@@ -74,34 +74,145 @@ elif visualizacion==etapas[1]:
     df_fecha_selec.rename(index={0:'Porcentaje'},inplace=True)
     st.bar_chart(df_fecha_selec[options].T,height=350,use_container_width=True)
 
-elif visualizacion==etapas[2]:
-    st.subheader('Cantidad recolectado')
-    df_recopilado = pd.read_csv('Elecciones + IA/data_base/data_base_csv/Fuentes.csv')
-    df_recopilado.sort_values(by='cant_publicaciones',inplace=True)
-    df_recopilado.rename(columns={'nombre':'Fuente','cant_publicaciones':'Publicaciones totales','cant_recopilado':'Publicaciones descargadas'},inplace=True)
-    st.bar_chart(df_recopilado,x ='Fuente',y=['Publicaciones totales','Publicaciones descargadas'],height=350,use_container_width=True)
+elif visualizacion==etapas[0]:
+    ################################# DATOS #######################################################
 
-    st.subheader('Publicaciones y me gusta')
-    
-    st.text("Cantidad de publicaciones")
     df_megusta_pub_pol = pd.read_csv('Elecciones + IA/modeling/Preparando_datos/publicaciones_politicas.csv')
     partidos = df_megusta_pub_pol['Partido'].unique()
-    df_cant_publi = df_megusta_pub_pol['Partido'].value_counts(ascending=True)
-    st.bar_chart(df_cant_publi)
+    fuentes = df_megusta_pub_pol['fuente'].unique()
+    colores = ['#A71FCC','#137DCC','#CCCA27','#D12437','#180D73']
 
-    st.text("Likes por partido")
-    df_megusta = df_megusta_pub_pol.groupby('Partido').sum()['cantidad_likes']
-    st.bar_chart(df_megusta)
+    options_fuente = st.multiselect(
+        'Fuentes: ',fuentes,
+        default=['infobae','lanacioncom', 'pagina12', 'c5n','filonewsok'],
+        placeholder='Elije una opción'
+        )
+
+    st.header('Publicaciones')
+    st.text('(Los datos fueron recopilados entre diciembre 2021 y noviembre 2023)')
+
+    df_recopilado = pd.read_csv('Elecciones + IA/data_base/data_base_csv/Fuentes.csv')
+    df_recopilado.sort_values(by='cant_publicaciones',inplace=True)
+
+    para_concatenar = []
+    for p in options_fuente:
+        para_concatenar.append(df_recopilado[df_recopilado['nombre']==p])
+    if len(para_concatenar)>0:
+        df_recopilado = pd.concat(para_concatenar)
+
+    aux = pd.concat([df_recopilado[['nombre','cant_publicaciones']],df_recopilado.rename(columns=dict(zip(df_recopilado.columns,['a','nombre','c','d','cant_publicaciones'])))[['nombre','cant_publicaciones']]],axis=0)
+    aux['Clase'] = len(df_recopilado)*['Publicaciones totales'] + len(df_recopilado)*['Cantidad descargada']
+
+    bar = alt.Chart(aux).mark_bar().encode(
+        alt.X('Clase').axis(labels=False,title=' '),
+        alt.Y('cant_publicaciones',type = 'quantitative').axis(title=' '),
+        alt.Column('nombre',type='nominal',title='Fuente'),
+        alt.Color('Clase',type = 'nominal').legend(orient='bottom',title=' '),
+        ).properties(
+            width=50,
+            height=200
+        )
+    
+    st.altair_chart(bar,use_container_width=False)
+
+    st.header('Publicaciones políticas')
+
+    politicasYNormales = pd.read_csv('Elecciones + IA/data_base/data_base_csv/politicasYnormales.csv')
+
+    para_concatenar = []
+    for p in options_fuente:
+        para_concatenar.append(politicasYNormales[politicasYNormales['Fuente']==p])
+    if len(para_concatenar)>0:
+        politicasYNormales = pd.concat(para_concatenar)
+
+    bar = alt.Chart(politicasYNormales).mark_bar().encode(
+        alt.X('Fuente').axis(labels=False,title=' '),
+        alt.Y('Cantidad',type = 'quantitative').axis(title=' '),
+        alt.Column('Tipo',type='nominal'),
+        alt.Color('Fuente',type = 'nominal').legend(orient='right',title=' '),
+        ).properties(
+            width=230,
+            height=300
+        )
+
+    st.altair_chart(bar,use_container_width=False)
+
+    partidosYFuentes = pd.read_csv('Elecciones + IA/data_base/data_base_csv/partidosYfuentes.csv')
+
+    st.subheader('Fuentes y publicaciones políticas')
+
+    para_concatenar = []
+    for p in options_fuente:
+        para_concatenar.append(partidosYFuentes[partidosYFuentes['fuente']==p])
+    if len(para_concatenar)>0:
+        partidosYFuentes = pd.concat(para_concatenar)
+
+    bar = alt.Chart(partidosYFuentes).mark_bar().encode(
+        alt.X('Partido').axis(labels=False,title=' '),
+        alt.Y('Cantidad',type = 'quantitative').axis(title=' '),
+        alt.Column('fuente',type='nominal'),
+        alt.Color('Partido',type = 'nominal').scale(domain=partidos, range=colores).legend(orient='bottom',title=' ',columns=3),
+        ).properties(
+            width=70,
+            height=220
+        )
+
+    st.altair_chart(bar,use_container_width=False)
+
+    st.header   ("Publicaciones y partidos políticos")
+
+    options_partidos = st.multiselect(
+        'Partido: ',partidos,
+        default=partidos[:3],
+        placeholder='Elije una opción'
+        )
+    
+    st.subheader("Cantidad de publicaciones")
+    unidades1 = st.radio(
+        "Unidades",
+        ["Porcentaje", "miles"]
+        )
+
+    para_concatenar = []
+    for p in options_partidos:
+        para_concatenar.append(df_megusta_pub_pol[df_megusta_pub_pol['Partido']==p])
+
+    if len(para_concatenar)>0:
+        df_megusta_pub_pol = pd.concat(para_concatenar)
+    df_cant_publi = df_megusta_pub_pol['Partido'].value_counts(ascending=True).reset_index()
+    df_megusta = df_megusta_pub_pol.groupby('Partido').sum()['cantidad_likes'].reset_index()
+    sum_cant_pub = df_cant_publi['Partido'].sum()
+    sum_cant_likes = df_megusta['cantidad_likes'].sum()
+
+    if unidades1=='Porcentaje':
+        df_cant_publi['cantidad'] = df_cant_publi['Partido'].map(lambda x: round(100*x/sum_cant_pub,1))
+    elif unidades1 == 'miles':
+        df_cant_publi['cantidad'] = df_cant_publi['Partido'].map(lambda x: round(x/1000,1))
+    
+    pie = alt.Chart(df_cant_publi).mark_arc(innerRadius=60,outerRadius=120).encode(
+        theta=alt.Theta(field="Partido", type="quantitative",stack=True),
+        color=alt.Color('index').scale(domain=partidos, range=colores).legend(orient='top-right',columns = 1)
+    )
+    text = pie.mark_text(radius=150, size=15).encode(text="cantidad")
+    
+    st.altair_chart(pie+text, theme=None, use_container_width=True)
+
+    st.subheader("Likes")
+    unidades2 = st.radio(
+        "Unidades",
+        ["Porcentaje", "millones"]
+    )
+    if unidades2=='Porcentaje':
+        df_megusta['cantidad'] = df_megusta['cantidad_likes'].map(lambda x: round(100*x/sum_cant_likes,1))
+    elif unidades2 == 'millones':
+        df_megusta['cantidad'] = df_megusta['cantidad_likes'].map(lambda x: round(x/1000000,1))
+    
+    pie = alt.Chart(df_megusta).mark_arc(innerRadius=60,outerRadius=120).encode(
+        theta=alt.Theta(field="cantidad_likes", type="quantitative",stack=True),
+        color=alt.Color('Partido').scale(domain=partidos, range=colores).legend(orient='top-right',columns = 1)
+    )
+    text = pie.mark_text(radius=150, size=15).encode(text="cantidad")
+
+    st.altair_chart(pie+text, theme=None, use_container_width=True)
 
 
-
-
-# radio widget to take inputs from mulitple options
-# partido_elegido = st.radio(
-#     "Partido: ",
-#     partidos)
-
-# if partido_elegido == 'Comedy':
-#     st.write('You selected comedy.')
-# else:
-#     st.write("You didn't select comedy.")
