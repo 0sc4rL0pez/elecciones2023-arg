96f4ae24ab2ff9355c044438c64c3ef5b89c2ca3
Oscar López <153029225+0sc4rL0pez@users.noreply.github.com>
2024-02-21 23:50:07 -0300

    Update app_En.py

--- app_En.py
+++ app_En.py
@@ -278,7 +278,7 @@ elif visualizacion==etapas[0]:
     
     pie = alt.Chart(df_megusta).mark_arc(innerRadius=60,outerRadius=120).encode(
         theta=alt.Theta(field="cantidad_likes", type="quantitative",stack=True),
-        color=alt.Color('Partido').scale(domain=partidos, range=colores).legend(orient='top-right',columns = 1)
+        color=alt.Color('Partido').scale(domain=partidos, range=colores).legend(orient='top-right',columns = 1,title='Party')
     )
     text = pie.mark_text(radius=150, size=15).encode(text="cantidad")
 
