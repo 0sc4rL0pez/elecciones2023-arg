802e259df7813452f6086a63343a4a43394280c5
Oscar López <153029225+0sc4rL0pez@users.noreply.github.com>
2024-02-21 23:41:26 -0300

    translating to english

--- app_En.py
+++ app_En.py
@@ -3,14 +3,14 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-etapas = ('Datos','Primera vuelta', 'Ballotage')
-visualizacion = st.sidebar.selectbox('Visualizar: ',etapas)
+etapas = ('Data','First phase', 'Second phase'')
+visualizacion = st.sidebar.selectbox('Visualize: ',etapas)
 
-st.header('**Elecciones + IA**')
+st.header('**Elections + IA**')
 
 if visualizacion==etapas[1]:
-    st.subheader('Primera vuelta')
-    st.text("Encuestas en función del tiempo (Wikipedia)")
+    st.subheader('First phase')
+    st.text("Surveys through  time (Wikipedia)")
 
     df = pd.read_csv('Elecciones + IA/dashboard/primera_vuelta_poly_encuestas.csv')
 
@@ -20,9 +20,8 @@ if visualizacion==etapas[1]:
 
     colores = ['#137DCC','#CCCA27','#A71FCC','#180D73','#D12437']
     options = st.multiselect(
-        'Partidos: ',partidos,
-        default=partidos[:3],
-        placeholder='Elije una opción')
+        'Political party: ',partidos,
+        default=partidos[:3])
     
     para_concatenar = []
     lista_partidos = []
@@ -31,16 +30,16 @@ if visualizacion==etapas[1]:
         para_concatenar += df[p].values.tolist()
         lista_partidos += len(df)*[p]
     if len(para_concatenar)>0:
-        df_aux['Porcentaje'] = para_concatenar
-        df_aux['Partido'] = lista_partidos
-        df_aux['Fecha'] = np.array(len(options)*df['Fecha'].values.tolist()).flatten()
+        df_aux['Percentage'] = para_concatenar
+        df_aux['Party'] = lista_partidos
+        df_aux['Date'] = np.array(len(options)*df['Fecha'].values.tolist()).flatten()
         #Estaria bueno poner la fecha en español!
         df_aux['Fecha'] = pd.to_datetime(df_aux['Fecha'],utc=True).dt.tz_convert(tz='America/Argentina/Buenos_Aires')
         
         linea = alt.Chart(df_aux).mark_circle(size=40).encode(
-            x='Fecha',
-            y='Porcentaje',
-            color = alt.Color('Partido').scale(domain=partidos, range=colores).legend(
+            x='Date',
+            y='Percentage',
+            color = alt.Color('Party').scale(domain=partidos, range=colores).legend(
                 orient='bottom',columns = 3,labelFontSize=12,symbolSize=120
             )
         ).properties(
@@ -49,36 +48,36 @@ if visualizacion==etapas[1]:
             )
         
         st.altair_chart(linea)
-        st.subheader('Modelo predictivo')
+        st.subheader('Machine learning model')
     df_predicc = pd.read_csv('Elecciones + IA/dashboard/predicciones_score_df_prim.csv')
     agrupado = df_predicc.groupby(['Inicio']).mean(numeric_only=True)[partidos + ['Scores']].reset_index()
     if len(options)>0:
         fecha_selec = st.select_slider(
-        'Fecha a predecir',
+        'Input date',
         options=agrupado['Inicio'].values.tolist())
 
         df_fecha_selec = agrupado[agrupado['Inicio']==fecha_selec]
-        st.text("Puntaje (-MSE): "+str(round(df_fecha_selec['Scores'].values[0],2))+" *")
+        st.text("Score (-MSE): "+str(round(df_fecha_selec['Scores'].values[0],2))+" *")
         df_fecha_selec = df_fecha_selec.rename(index={0:'Porcentaje'})
     
     porcentajes = []
     for p in options:
         porcentajes.append(df_fecha_selec[p].values.tolist())
     aux_df = pd.DataFrame()
-    aux_df['porcentajes'] = np.round(np.array(porcentajes).flatten())
-    aux_df['Partido'] = options
+    aux_df['Percentage'] = np.round(np.array(porcentajes).flatten())
+    aux_df['Party'] = options
     if len(options)>0:
         pie = alt.Chart(aux_df).mark_arc(innerRadius=60,outerRadius=120).encode(
-                theta=alt.Theta(field="porcentajes", type="quantitative",stack=True),
-                color=alt.Color('Partido').scale(domain=partidos, range=colores).legend(orient='top-right',columns = 1)
+                theta=alt.Theta(field="Percentage", type="quantitative",stack=True),
+                color=alt.Color('Party').scale(domain=partidos, range=colores).legend(orient='top-right',columns = 1)
         )
-        text = pie.mark_text(radius=150, size=15).encode(text="porcentajes")
+        text = pie.mark_text(radius=150, size=15).encode(text="Percentage")
     
         st.altair_chart(pie+text, theme=None, use_container_width=True)
-    st.caption("*MSE: Error cuadrático medio")
+    st.caption("*MSE: Mean squared error")
 elif visualizacion==etapas[2]:
-    st.subheader('Ballotaje')
-    st.text("Encuestas en función del tiempo (Wikipedia)")
+    st.subheader('Second phase')
+    st.text("Surveys through  time (Wikipedia)")
 
     df = pd.read_csv('Elecciones + IA/getting_data/encuestas/Encuestas_solo_ballotaje.csv')
 
@@ -88,9 +87,8 @@ elif visualizacion==etapas[2]:
 
     colores = ['#A71FCC','#137DCC']
     options = st.multiselect(
-        'Partidos: ',partidos,
-        default=partidos[:2],
-        placeholder='Elije una opción')
+        'Party: ',partidos,
+        default=partidos[:2])
 
     para_concatenar = []
     lista_partidos = []
@@ -99,15 +97,15 @@ elif visualizacion==etapas[2]:
         para_concatenar += df[p].values.tolist()
         lista_partidos += len(df)*[p]
     if len(para_concatenar)>0:
-        df_aux['Porcentaje'] = para_concatenar
-        df_aux['Partido'] = lista_partidos
+        df_aux['Percentage'] = para_concatenar
+        df_aux['Party'] = lista_partidos
         df_aux['Fecha'] = np.array(len(options)*df['Fecha'].values.tolist()).flatten()
-        df_aux['Fecha'] = pd.to_datetime(df_aux['Fecha'])
+        df_aux['Date'] = pd.to_datetime(df_aux['Fecha'])
         
         linea = alt.Chart(df_aux).mark_circle(size=40).encode(
-            x='Fecha',
-            y='Porcentaje',
-            color = alt.Color('Partido').scale(domain=partidos, range=colores).legend(
+            x='Date',
+            y='Percentage',
+            color = alt.Color('Party').scale(domain=partidos, range=colores).legend(
                 orient='bottom',columns = 3,labelFontSize=12,symbolSize=120
             )
         ).properties(
@@ -117,12 +115,12 @@ elif visualizacion==etapas[2]:
         
         st.altair_chart(linea)
     
-        st.subheader('Modelo predictivo')
+        st.subheader('Machine learning model')
     df_predicc = pd.read_csv('Elecciones + IA/dashboard/predicciones_score_ballotaje.csv')
     agrupado = df_predicc.groupby(['Inicio']).mean(numeric_only=True)[partidos + ['Scores']].reset_index()
     if len(options)>0:
         fecha_selec = st.select_slider(
-        'Fecha a predecir',
+        'Input date',
         options=agrupado['Inicio'].values.tolist())
 
         df_fecha_selec_aux = agrupado[agrupado['Inicio']==fecha_selec]
@@ -133,17 +131,17 @@ elif visualizacion==etapas[2]:
     for p in options:
         porcentajes.append(df_fecha_selec[p].values.tolist())
     aux_df = pd.DataFrame()
-    aux_df['porcentajes'] = np.round(np.array(porcentajes).flatten(),2)
-    aux_df['Partido'] = options
+    aux_df['Percentage'] = np.round(np.array(porcentajes).flatten(),2)
+    aux_df['Party'] = options
     if len(options)>0:
         pie = alt.Chart(aux_df).mark_arc(innerRadius=60,outerRadius=120).encode(
-                theta=alt.Theta(field="porcentajes", type="quantitative",stack=True),
-                color=alt.Color('Partido').scale(domain=partidos, range=colores).legend(orient='top-right',columns = 1)
+                theta=alt.Theta(field="Percentage", type="quantitative",stack=True),
+                color=alt.Color('Party').scale(domain=partidos, range=colores).legend(orient='top-right',columns = 1)
         )
-        text = pie.mark_text(radius=150, size=15).encode(text="porcentajes")
+        text = pie.mark_text(radius=150, size=15).encode(text="Percentage")
     
         st.altair_chart(pie+text, theme=None, use_container_width=True)
-    st.caption("*MSE: Error cuadrático medio")
+    st.caption("*MSE: Mean squared error")
 
 elif visualizacion==etapas[0]:
     ################################# DATOS #######################################################
@@ -154,13 +152,12 @@ elif visualizacion==etapas[0]:
     colores = ['#A71FCC','#137DCC','#CCCA27','#D12437','#180D73']
 
     options_fuente = st.multiselect(
-        'Fuentes: ',fuentes,
-        default=['infobae','lanacioncom', 'pagina12', 'c5n','filonewsok'],
-        placeholder='Elije una opción'
+        'Source: ',fuentes,
+        default=['infobae','lanacioncom', 'pagina12', 'c5n','filonewsok']
         )
 
-    st.header('Publicaciones')
-    st.text('(Los datos fueron recopilados entre diciembre 2021 y noviembre 2023)')
+    st.header('Posts')
+    st.text('(The data has been scraped between december 2021 and november 2023)')
 
     df_recopilado = pd.read_csv('Elecciones + IA/data_base/data_base_csv/Fuentes.csv')
     df_recopilado.sort_values(by='cant_publicaciones',inplace=True)
@@ -172,13 +169,13 @@ elif visualizacion==etapas[0]:
         df_recopilado = pd.concat(para_concatenar)
 
     aux = pd.concat([df_recopilado[['nombre','cant_publicaciones']],df_recopilado.rename(columns=dict(zip(df_recopilado.columns,['a','nombre','c','d','cant_publicaciones'])))[['nombre','cant_publicaciones']]],axis=0)
-    aux['Clase'] = len(df_recopilado)*['Publicaciones totales'] + len(df_recopilado)*['Cantidad descargada']
+    aux['Type'] = len(df_recopilado)*['Publicaciones totales'] + len(df_recopilado)*['Cantidad descargada']
 
     bar = alt.Chart(aux).mark_bar().encode(
-        alt.X('Clase').axis(labels=False,title=' '),
+        alt.X('Type').axis(labels=False,title=' '),
         alt.Y('cant_publicaciones',type = 'quantitative').axis(title=' '),
-        alt.Column('nombre',type='nominal',title='Fuente'),
-        alt.Color('Clase',type = 'nominal').legend(orient='bottom',title=' '),
+        alt.Column('nombre',type='nominal',title='Source'),
+        alt.Color('Type',type = 'nominal').legend(orient='bottom',title=' '),
         ).properties(
             width=50,
             height=200
@@ -186,10 +183,10 @@ elif visualizacion==etapas[0]:
     
     st.altair_chart(bar,use_container_width=False)
 
-    st.header('Publicaciones políticas*')
+    st.header('Political posts*')
 
     politicasYNormales = pd.read_csv('Elecciones + IA/data_base/data_base_csv/politicasYnormales.csv')
-
+    politicasYNormales = politicasYNormales.rename(columns={'Tipo':'Type', 'Fuente':'Source'})
     para_concatenar = []
     for p in options_fuente:
         para_concatenar.append(politicasYNormales[politicasYNormales['Fuente']==p])
@@ -197,10 +194,10 @@ elif visualizacion==etapas[0]:
         politicasYNormales = pd.concat(para_concatenar)
 
     bar = alt.Chart(politicasYNormales).mark_bar().encode(
-        alt.X('Fuente').axis(labels=False,title=' '),
+        alt.X('Source').axis(labels=False,title=' '),
         alt.Y('Cantidad',type = 'quantitative').axis(title=' '),
-        alt.Column('Tipo',type='nominal'),
-        alt.Color('Fuente',type = 'nominal').legend(orient='right',title=' '),
+        alt.Column('Type',type='nominal'),
+        alt.Color('Source',type = 'nominal').legend(orient='right',title=' '),
         ).properties(
             width=230,
             height=300
@@ -209,19 +206,20 @@ elif visualizacion==etapas[0]:
     st.altair_chart(bar,use_container_width=False)
 
     partidosYFuentes = pd.read_csv('Elecciones + IA/data_base/data_base_csv/partidosYfuentes.csv')
-    st.caption("*Una publicación es política si en la descripción del mismo se encuentran al menos un apellido o nombre completo de alguno candidatos  o el nombre del partido en cuestión")
-    st.subheader('Fuentes y publicaciones políticas')
+    st.caption("*A post is political if in its caption there is at least one last name or full name of any candidate or the name of the party in question")
+    st.subheader('Sources and political posts')
 
     para_concatenar = []
     for p in options_fuente:
         para_concatenar.append(partidosYFuentes[partidosYFuentes['fuente']==p])
     if len(para_concatenar)>0:
         partidosYFuentes = pd.concat(para_concatenar)
-
+    
+    partidosYFuentes = partidosYFuentes.rename(columns={'Partido':'Party', 'fuente':'Source'})
     bar = alt.Chart(partidosYFuentes).mark_bar().encode(
         alt.X('Partido').axis(labels=False,title=' '),
         alt.Y('Cantidad',type = 'quantitative').axis(title=' '),
-        alt.Column('fuente',type='nominal'),
+        alt.Column('Source',type='nominal'),
         alt.Color('Partido',type = 'nominal').scale(domain=partidos, range=colores).legend(orient='bottom',title=' ',columns=3),
         ).properties(
             width=70,
@@ -230,18 +228,17 @@ elif visualizacion==etapas[0]:
 
     st.altair_chart(bar,use_container_width=False)
 
-    st.header("Publicaciones y partidos políticos")
+    st.header("Posts and political post")
 
     options_partidos = st.multiselect(
         'Partido: ',partidos,
-        default=partidos[:3],
-        placeholder='Elije una opción'
+        default=partidos[:3]
         )
     
-    st.subheader("Cantidad de publicaciones")
+    st.subheader("Total posts")
     unidades1 = st.radio(
-        "Unidades",
-        ["Porcentaje", "miles"]
+        "Unit of measurement",
+        ["Percentage", "thousands"]
         )
 
     para_concatenar = []
@@ -271,8 +268,8 @@ elif visualizacion==etapas[0]:
 
     st.subheader("Likes")
     unidades2 = st.radio(
-        "Unidades",
-        ["Porcentaje", "millones"]
+        "Unit of measurement",
+        ["Percentage", "millions"]
     )
     if unidades2=='Porcentaje':
         df_megusta['cantidad'] = df_megusta['cantidad_likes'].map(lambda x: round(100*float(x)/sum_cant_likes,1))
