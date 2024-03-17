e90537eb3bdace9daeea0945dc5dd35b58b091ef
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-03-17 12:35:58 -0300

    cleaning code (not finished)

--- Elecciones + IA/getting_data/surveys/cleaning_df.py
+++ Elecciones + IA/getting_data/surveys/cleaning_df.py
@@ -3,47 +3,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-# # Cobertura de las encuestas
+parties = ['Union por la Patria','Juntos por el Cambio', 'La Libertad Avanza','Hacemos juntos nuestro Pais', 'Frente de Izquierda y Trabajadores']
+primera_vuelta = pd.read_csv('Elecciones + IA/getting_data/surveys/data_scraped/Survey_primera_vuelta.csv')
+ballotage = pd.read_csv('Elecciones + IA/getting_data/surveys/data_scraped/Survey_ballotage.csv')
 
-partidos = ['Union por la Patria','Juntos por el Cambio', 'La Libertad Avanza','Hacemos juntos nuestro Pais', 'Frente de Izquierda y Trabajadores']
-df = pd.read_csv('FILL')
-
-fig, ax = plt.subplots(3,2,figsize=(10,10))
-ploteos = ax.flatten()
-for i,p in enumerate(partidos):
-    sns.histplot(df[p],bins=50, kde=True,ax= ploteos[i])
-
-
-otros = ['Otros', 'Blanco', 'Indecisos', 'Ventaja']
-fig, ax = plt.subplots(2,2,figsize=(7,7))
-ploteos = ax.flatten()
-for i,p in enumerate(otros):
-    sns.histplot(df[p],bins=50, kde=True,ax= ploteos[i])
+#plotting need to be moved out!
+# fig, ax = plt.subplots(3,2,figsize=(10,10))
+# ploteos = ax.flatten()
+# for i,p in enumerate(parties):
+#     sns.histplot(df[p],bins=50, kde=True,ax= ploteos[i])
 
 
-sin_outliers = df[(df['Muestra']<30000) & (df['Muestra']>0)]['Muestra']
-sns.histplot(sin_outliers,bins=50, kde=True)
+# otros = ['Otros', 'Blanco', 'Indecisos', 'Ventaja']
+# fig, ax = plt.subplots(2,2,figsize=(7,7))
+# ploteos = ax.flatten()
+# for i,p in enumerate(otros):
+#     sns.histplot(df[p],bins=50, kde=True,ax= ploteos[i])
 
 
-tibios = df['Indecisos']+df['Blanco']
-sns.histplot(tibios,bins=50, kde=True)
+#with_out_outliers = primera_vuelta[(primera_vuelta['Muestra']<30000) & (primera_vuelta['Muestra']>0)]['Muestra']
+
+careless = primera_vuelta['Indecisos']+primera_vuelta['Blanco']
 
 
-tibios.quantile(0.90)
-
-
-# Filtremos los tibios con la cobertura del 90% de los datos
-
-
-df  = df.loc[tibios<22]
-
-
-# IDEA  :Dejar los valores de las encuestas en una sola columna, hay que eliminar aquellos que son '-' (DESCARTADO)
+limit = careless.quantile(0.90)
+primera_vuelta  = primera_vuelta.loc[careless<limit]
 
 
 # Guardo solo las columas que me interesan
-columnas = partidos + ['Inicio', 'Final']
-clean = df.loc[:,columnas]
+columnas = parties + ['Inicio', 'Final']
+clean = primera_vuelta.loc[:,columnas]
 clean.to_csv('C:/Users/54911/OneDrive/Escritorio/Data Science/Elecciones + IA/getting_data/encuestas/Encuestas_primera_vuelta.csv',
              index= False)
 
@@ -52,6 +41,7 @@ ballotage = pd.read_csv('FILL')
 ballotage.dropna(inplace=True)
 
 otros = ['Blanco', 'Indecisos']
+
 fig, ax = plt.subplots(1,2,figsize=(12,5))
 ploteos = ax.flatten()
 for i,p in enumerate(otros):
@@ -60,7 +50,7 @@ for i,p in enumerate(otros):
 
 ballotage = ballotage[ballotage['Indecisos']<20]
 
-labelss = ['La Libertad Avanza','Union por la Patria','Inicio','Final']
+labels = ['La Libertad Avanza','Union por la Patria','Inicio','Final']
 
-ballotage = ballotage[labelss]
+ballotage = ballotage[labels]
 
