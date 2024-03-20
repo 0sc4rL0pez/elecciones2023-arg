8c998d221191ec59755d494bd3974081374ba41a
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-03-20 20:34:00 -0300

    utilities data base for ploting

--- Elecciones + IA/analisis_exploratorio/utilities.py
+++ Elecciones + IA/analisis_exploratorio/utilities.py
@@ -1,6 +1,52 @@
-import pandas as pd
-import matplotlib.pyplot as plt
 import pyodbc
-import seaborn as sns
-import datetime
-import numpy as np
+import pandas as pd
+
+def conectar_base_datos():
+    SERVER = 'ACHEPE'
+    DATABASE = 'elecciones-IA-data'
+
+    connectionString = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection=yes;'
+
+    conn = pyodbc.connect(connectionString) 
+    return conn
+
+
+def consultar(sql_statement):
+    conn = conectar_base_datos()
+    cursor = conn.cursor()
+    SQL_STATEMENT = f"""
+        {sql_statement}
+    """
+    consulta = cursor.execute(SQL_STATEMENT) 
+    return consulta 
+
+def pasar_a_DF(dat_cursor):
+    columnas = dat_cursor.description
+    nombre = []
+    datos = []
+
+    for i in range(len(columnas)):
+        nombre.append(columnas[i][0])
+        datos.append([])
+
+    for fila in dat_cursor:
+        for i in range(len(columnas)):
+            datos[i].append(fila[i])
+    
+    diccionario = dict()
+    for i in range(len(columnas)):
+        diccionario[nombre[i]] = datos[i]
+
+    df = pd.DataFrame(diccionario)
+
+    return df
+
+
+'''
+SQL_STATEMENT = f"""
+       SELECT *
+       FROM Publicaciones
+"""
+data = consultar(SQL_STATEMENT)
+df_publicaciones = pasar_a_DF(data)
+'''
