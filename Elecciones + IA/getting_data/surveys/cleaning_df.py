7d329cc31d9f646f72a4f07cc3fac984ef52a8f0
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-03-21 12:35:32 -0300

    saving clean surveys data

--- Elecciones + IA/getting_data/surveys/cleaning_df.py
+++ Elecciones + IA/getting_data/surveys/cleaning_df.py
@@ -3,33 +3,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-parties = ['Union por la Patria','Juntos por el Cambio', 'La Libertad Avanza','Hacemos juntos nuestro Pais', 'Frente de Izquierda y Trabajadores']
 primera_vuelta = pd.read_csv('Elecciones + IA/getting_data/surveys/data_scraped/Survey_primera_vuelta.csv')
 ballotage = pd.read_csv('Elecciones + IA/getting_data/surveys/data_scraped/Survey_ballotage.csv')
 
 #Check the analysis in : Elecciones + IA\analisis_exploratorio\surveys_plots.ipynb
 
-#cleaning
+#cleaning and saving only the columns that matters
 
 careless = primera_vuelta['Indecisos']+primera_vuelta['Blanco']
-
-# Saving only the columns that matters
-columnas = parties + ['Inicio', 'Final']
-clean = primera_vuelta.loc[:,columnas]
-clean.to_csv('C:/Users/54911/OneDrive/Escritorio/Data Science/Elecciones + IA/getting_data/encuestas/Encuestas_primera_vuelta.csv',
-             index= False)
-
-ballotage = pd.read_csv('FILL')
-ballotage.dropna(inplace=True)
-
-otros = ['Blanco', 'Indecisos']
+filtering_outliers = (primera_vuelta['Muestra']>=1000) & (primera_vuelta['Muestra']<5000) & (careless<=20)
 
 
+parties = ['Union por la Patria','Juntos por el Cambio', 'La Libertad Avanza','Hacemos juntos nuestro Pais', 'Frente de Izquierda y Trabajadores']
+columns = parties + ['Inicio', 'Final']
+primera_vuelta_cleaned = primera_vuelta[filtering_outliers].loc[:,columns]
+primera_vuelta_cleaned.to_csv('Elecciones + IA/getting_data/surveys/data_scraped/primera_vuelta_cleaned.csv',index= False)
 
 
-ballotage = ballotage[ballotage['Indecisos']<20]
-
+careless = ballotage['Indecisos']+ballotage['Blanco']
+filtering_outliers = (ballotage['Muestra']>=2000) & (ballotage['Muestra']<14000) & (careless<18)
 labels = ['La Libertad Avanza','Union por la Patria','Inicio','Final']
+ballotage_cleaned = ballotage[filtering_outliers].loc[:,labels]
+ballotage_cleaned.to_csv('Elecciones + IA/getting_data/surveys/data_scraped/ballotage_cleaned.csv',index= False)
 
-ballotage = ballotage[labels]
 
