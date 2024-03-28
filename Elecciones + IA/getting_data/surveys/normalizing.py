400f3998c1af73b9ea259bb1955e8042f5b0739f
Oscar López <lopezcoroneloscarmanuel@gmailcom>
2024-03-28 12:11:54 -0300

    code to normalize surveys values

---
+++ Elecciones + IA/getting_data/surveys/normalizing.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+primera_vuelta_clean = pd.read_csv('Elecciones + IA/getting_data/surveys/data_scraped/primera_vuelta_cleaned.csv')
+ballojate_clean = pd.read_csv('Elecciones + IA/getting_data/surveys/data_scraped/ballotage_cleaned.csv')
